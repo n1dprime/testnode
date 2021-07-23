22fae6b320151822226431f5796892ba001cce09
charbel.k <charbel.k@euriskomobility.com>
2021-07-23 13:36:06 +0300

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const url = require('url');
 
 const hostname = '0.0.0.0';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
     var url_parts = url.parse(req.url, true);
